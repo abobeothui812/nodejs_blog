02cbd19ac027ec2d749bb5fd2ed290bd675e2b9f
Duc Anh <khamvl08@gmail.com>
2024-10-14 23:09:26 +0700

    hello

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express();
+const port = 3000;
+const morgan = require('morgan');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', handlebars.engine({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', './views');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+console.log(path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/news', (req, res) => {
+    res.render('news');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
